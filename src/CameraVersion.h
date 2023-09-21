19b08630a4a8917b300300d5ffd45c4596db2091
ConstantRobotics <s.zaplatnikov@constantrobotics.com>
2023-09-21 09:19:11 +0200

    Changed version

--- src/CameraVersion.h
+++ src/CameraVersion.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define CAMERA_MAJOR_VERSION 2
-#define CAMERA_MINOR_VERSION 1
+#define CAMERA_MINOR_VERSION 2
 #define CAMERA_PATCH_VERSION 0
 
-#define CAMERA_VERSION "2.1.0"
+#define CAMERA_VERSION "2.2.0"
